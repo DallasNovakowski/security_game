5bfe88cbb18ff4cebee1bd28e6e1fcea3776e441
Dallas Novakowski <47903855+DallasNovakowski@users.noreply.github.com>
2021-10-12 09:11:19 -0700

    insert time spent for iu

--- questionnaires/__init__.py
+++ questionnaires/__init__.py
@@ -90,7 +90,7 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-
+    ts_iu = models.FloatField(blank=True)
     iu_1 = make_iu("Unforeseen events upset me greatly")
     iu_2 = make_iu("It frustrates me not having all the information I need.")
     iu_3 = make_iu("Uncertainty keeps me from living a full life.")
@@ -104,6 +104,7 @@ class Player(BasePlayer):
     iu_11 = make_iu("I should be able to organize everything in advance.")
     iu_12 = make_iu("I must get away from all uncertain situations.")
 
+    ts_dospert = models.FloatField(blank=True)
     dospert_1 = make_dospert("Admitting that your tastes are different from those of a friend. ")
     dospert_2 = make_dospert("Going camping in the wilderness.")
     dospert_3 = make_dospert("Betting a day’s income at the horse races.")
@@ -140,7 +141,7 @@ class Player(BasePlayer):
 # PAGES
 class IU(Page):
     form_model = 'player'
-    form_fields = ['iu_1', 'iu_2', 'iu_3', 'iu_4', 'iu_5', 'iu_6', 'iu_7', 'iu_8', 'iu_9', 'iu_10', 'iu_11', 'iu_12']
+    form_fields = ['iu_1', 'iu_2', 'iu_3', 'iu_4', 'iu_5', 'iu_6', 'iu_7', 'iu_8', 'iu_9', 'iu_10', 'iu_11', 'iu_12', 'ts_iu']
 
 
     @staticmethod
@@ -159,16 +160,6 @@ class DOSPERT(Page):
                    'dospert_17','dospert_18','dospert_19','dospert_20','dospert_21',
                    'dospert_22','dospert_23','dospert_24','dospert_25','dospert_26',
                    'dospert_27','dospert_28','dospert_29','dospert_30']
-
-
-
-
-    # use this function for resetting submit_missing
-    # @staticmethod
-    # def before_next_page(self, timeout_happened):
-    #     self.prolific_id = self.participant.label
-    # pass
-
     pass
 
 page_sequence = [IU,DOSPERT]
